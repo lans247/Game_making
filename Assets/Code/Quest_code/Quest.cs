edc9a069dff41a69272a00283e236884586d2c76
sinhu0603 <157285791+sinhu0603@users.noreply.github.com>
2024-03-02 16:27:13 +0900

    test

---
+++ Assets/Code/Quest_code/Quest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Quest : ScriptableObject
+{
+    int ID;
+    public string Description;
+    public int type;
+
+    public string Title;
+    public string reward;
+}
